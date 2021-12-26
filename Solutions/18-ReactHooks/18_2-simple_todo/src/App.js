c0c15f9d7b5e005cd577d3f44ba1610d922a2bb4
Yitz <chalfari@gmail.com>
2021-12-26 12:25:13 +0200

    finished todo

--- Solutions/18-ReactHooks/18_2-simple_todo/src/App.js
+++ Solutions/18-ReactHooks/18_2-simple_todo/src/App.js
@@ -1,7 +1,38 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./App.css";
+const data = [
+  { name: "CSS", completed: true },
+  { name: "JavaScript", completed: true },
+  { name: "Learn React", completed: false },
+  { name: "Learn mongoDB", completed: false },
+  { name: "Learn Node JS", completed: false },
+];
 const App = () => {
-  return <div></div>;
+  const [list, setList] = useState([...data]);
+  const toggleCompleted = (i) => {
+    const temp = [...list];
+    temp[i].completed = !temp[i].completed;
+    setList([...temp]);
+  };
+  const renderList = () =>
+    list.map((li, i) => (
+      <div key={li.name} onClick={() => toggleCompleted(i)}>
+        <h4 className={li.completed ? "completed" : null}>
+          {li.name}{" "}
+          <span>
+            {li.completed
+              ? String.fromCharCode(2713)
+              : String.fromCharCode(2715)}
+          </span>
+        </h4>
+      </div>
+    ));
+  return (
+    <div>
+      <h1>WHY IS String.fromCharCode(2713) NOT GIVING ME A CHECK MARK?</h1>
+      {renderList()}
+    </div>
+  );
 };
 
 export default App;
---
+++ Solutions/18-ReactHooks/18_3-whats_the_time/src/Input.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Input = ({value,type}) => {
+  return (
+    <>
+      <label htmlFor={}></label>
+      <input value={value} type="text" />
+    </>
+  )
+}
+
+export default Input
