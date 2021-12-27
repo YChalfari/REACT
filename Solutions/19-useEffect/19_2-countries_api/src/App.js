e729f141cadb4c9dffe4af3c83d5ee40fc01f292
Yitz <chalfari@gmail.com>
2021-12-27 13:49:48 +0200

    19.2 completed but case sens

--- Solutions/19-useEffect/19_2-countries_api/src/App.js
+++ Solutions/19-useEffect/19_2-countries_api/src/App.js
@@ -1,7 +1,40 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const PROXY = "https://intense-mesa-62220.herokuapp.com/";
+const MOVIE_LINK = "https://restcountries.com/v2/all";
 const App = () => {
-  return <div></div>;
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [term, setTerm] = useState("");
+  const handleChange = (e) => {
+    setTerm(e.target.value);
+  };
+  useEffect(() => {
+    const filtered = countries.filter((country) => country.name.includes(term));
+    setFilteredCountries(filtered);
+  }, [term]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await axios.get(`${PROXY}${MOVIE_LINK}`);
+      console.log(data.data);
+      setCountries(data.data);
+    })();
+  }, []);
+  const renderCountries = () => {
+    if (filteredCountries.length > 0) {
+      return filteredCountries.map((country, i) => {
+        return <li key={country.name}>{country.name}</li>;
+      });
+    }
+  };
+  return (
+    <div>
+      <label>Search for a country: </label>
+      <input type="text" value={term} onChange={handleChange} />
+      <ul>{renderCountries()}</ul>
+    </div>
+  );
 };
 
 export default App;
