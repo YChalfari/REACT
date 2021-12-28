bdb7a24c57a2a3c96db8a317891950b458fde27c
Yitz <chalfari@gmail.com>
2021-12-28 13:51:35 +0200

    21.1 complete

--- Solutions/21-Hooks-UseRef/21_1-editing/src/App.js
+++ Solutions/21-Hooks-UseRef/21_1-editing/src/App.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import Fetch from "./Fetch";
 
 function App() {
   const [toggle, setToggle] = useState(false);
-
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current && inputRef.current.focus();
+  }, [toggle]);
   return (
     <div className="App">
-      <button onClick={() => setToggle((prev) => !prev)}>Toggle</button>
-      {toggle && <Fetch />}
+      <button onClick={() => setToggle((prev) => !prev)}>
+        {!toggle ? "Edit" : "Save"}
+      </button>
+      {toggle && <input ref={inputRef} type="text" />}
     </div>
   );
 }
